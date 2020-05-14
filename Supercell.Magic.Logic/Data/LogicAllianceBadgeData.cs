cfa55ad68c9d329cb978564ab78657ae70360e24
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2020-05-14 16:04:33 +0300

    Update LogicAllianceBadgeData.cs

--- Supercell.Magic.Logic/Data/LogicAllianceBadgeData.cs
+++ Supercell.Magic.Logic/Data/LogicAllianceBadgeData.cs
@@ -17,9 +17,14 @@ namespace Supercell.Magic.Logic.Data
         {
             base.CreateReferences();
 
-            this.m_iconLayer0 = this.GetValue("IconLayer0", 0);
-            this.m_iconLayer1 = this.GetValue("IconLayer1", 0);
-            this.m_iconLayer2 = this.GetValue("IconLayer2", 0);
+            this.m_iconLayer0 = LogicDataTables.GetAllianceBadgeLayerByName(this.GetValue("IconLayer0", 0), this);
+            this.m_iconLayer1 = LogicDataTables.GetAllianceBadgeLayerByName(this.GetValue("IconLayer1", 0), this);
+            this.m_iconLayer2 = LogicDataTables.GetAllianceBadgeLayerByName(this.GetValue("IconLayer2", 0), this);
+        }
+
+        public LogicAllianceBadgeLayerData GetIconLayer()
+        {
+            return this.m_iconLayer0 + this.m_iconLayer1 + this.m_iconLayer2;
         }
     }
 }
