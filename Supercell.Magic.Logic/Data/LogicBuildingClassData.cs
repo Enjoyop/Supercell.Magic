5f736e6e90e3e0151bfb7f32bf201db8ad9c75cc
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2021-04-25 12:43:07 +0300

    Update LogicBuildingClassData.cs

--- Supercell.Magic.Logic/Data/LogicBuildingClassData.cs
+++ Supercell.Magic.Logic/Data/LogicBuildingClassData.cs
@@ -12,6 +12,8 @@ namespace Supercell.Magic.Logic.Data
         private bool m_shopCategoryResource;
         private bool m_shopCategoryArmy;
         private bool m_shopCategoryDefense;
+        
+        private LogicBuildingClassData m_parentClass;
 
         public LogicBuildingClassData(CSVRow row, LogicDataTable table) : base(row, table)
         {
@@ -26,6 +28,7 @@ namespace Supercell.Magic.Logic.Data
             this.m_shopCategoryResource = this.GetBooleanValue("ShopCategoryResource", 0);
             this.m_shopCategoryArmy = this.GetBooleanValue("ShopCategoryArmy", 0);
             this.m_shopCategoryDefense = this.GetBooleanValue("ShopCategoryDefense", 0);
+            this.m_parentClass = LogicDataTables.GetBuildingClassByName(this.GetValue("ParentClass", 0), this);
 
             this.m_workerClass = string.Equals("Worker", this.GetName());
 
@@ -62,6 +65,11 @@ namespace Supercell.Magic.Logic.Data
         public bool CanBuy()
         {
             return this.m_canBuy;
+        }
+        
+        public LogicBuildingClassData GetParentClass()
+        {
+            return this.m_parentClass;
         }
     }
 }
